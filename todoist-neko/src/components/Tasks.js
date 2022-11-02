77bab52476132c5a786b528b719d9f8109a71692
nutdanai-kpjr <nutdanai.kpjr@gmail.com>
2022-11-02 13:50:36 +0700

    Add index page structure

---
+++ todoist-neko/src/components/Tasks.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+export const Tasks = () => {
+  return (
+    <div className="tasks">
+      <div>Tasks</div>
+    </div>
+  );
+};
--- todoist-neko/src/components/layout/Content.js
+++ todoist-neko/src/components/layout/Content.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { Sidebar } from "./Sidebar";
 export const Content = () => {
   return (
-    <div>
+    <div className="content">
       <Sidebar />
       <div>Content</div>
+      <Tasks></Tasks>
     </div>
   );
 };
